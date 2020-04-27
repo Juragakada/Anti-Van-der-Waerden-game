629119cccc41c37ff8290d826db4b2ea42028f23
Juragakada <61868621+Juragakada@users.noreply.github.com>
2020-04-27 12:58:38 +0200

    Improvements

--- board.hpp
+++ board.hpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include <vector>
 
-using std::vector;
+using namespace std;
 
 class board{
 public:
@@ -14,21 +14,23 @@ public:
     vector<int> board;
     vector<int> colours;
 
-    void intput_data(int n,int k, int c);
 
-    void create_board();
+    void create_board(int size,vector<int> &board);
 
-    void create_board(int number);
+    void create_board(int size,vector<int> &board,int number);
 
-    void clear_board();
+    void clear_board(vector<int>& board, int lenght);
 
-    void clear_board( int number);
+    void clear_board(vector<int>& board, int lenght, int number);
 
     void color_the_field(int n,int c);
     
-    bool check_if_win(vector<int> board,int WinLenght,int ColoursNumbers, int last_move);
+    bool check_if_win(int last_move);
+
+    void print_board(vector<int> board);
 
 };
+
 
 
 
